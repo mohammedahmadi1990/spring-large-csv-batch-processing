984a345d16b2167c67f82690b03d0b5c45dc5578
Mohammed <mohammed.ahmadi1990@gmail.com>
2022-11-23 01:28:34 +0300

    Initial Commit
    Initialization of SpringBoot package

---
+++ csv-processor/src/main/java/com/design/csvprocessor/CsvProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.design.csvprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvProcessorApplication.class, args);
+	}
+
+}
---
+++ csv-processor/src/test/java/com/design/csvprocessor/CsvProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.design.csvprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
